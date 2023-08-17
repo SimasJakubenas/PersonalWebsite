294d96cb61832a9a873f37ae6870fb57bf51acbd
sjakubenas <sjakubenas@gmail.com>
2023-08-17 11:10:57 -0400

    Replace menu toggle javascript with inline jquery for better funtion

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-/* Toggles between menu icon and menu list by assigning "responsive" class to navbar*/
-function myFunction() {
-    var x = document.getElementById("menu");
-    if (x.className === "navbar") {
-      x.className += " responsive";
-    } else {
-      x.className = "navbar";
-    }
-  }
